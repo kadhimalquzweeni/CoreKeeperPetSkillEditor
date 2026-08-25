16cf7e0e364c6201f2623885b51534edc4560fda
Kadhim <kadhimalquzweeni@gmail.com>
2026-08-25 13:46:07 +0100

    Updated the UI

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
             tlpTalents = new TableLayoutPanel();
             cmbTalent9 = new ComboBox();
             cmbTalent8 = new ComboBox();
@@ -60,8 +61,8 @@
             // 
             tlpTalents.AutoSize = true;
             tlpTalents.ColumnCount = 2;
-            tlpTalents.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 55F));
-            tlpTalents.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 100F));
+            tlpTalents.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 57F));
+            tlpTalents.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 16F));
             tlpTalents.Controls.Add(cmbTalent9, 1, 8);
             tlpTalents.Controls.Add(cmbTalent8, 1, 7);
             tlpTalents.Controls.Add(cmbTalent7, 1, 6);
@@ -80,7 +81,7 @@
             tlpTalents.Controls.Add(lblTalent2, 0, 1);
             tlpTalents.Controls.Add(lblTalent1, 0, 0);
             tlpTalents.Controls.Add(cmbTalent1, 1, 0);
-            tlpTalents.Location = new Point(0, 149);
+            tlpTalents.Location = new Point(12, 149);
             tlpTalents.Name = "tlpTalents";
             tlpTalents.RowCount = 9;
             tlpTalents.RowStyles.Add(new RowStyle(SizeType.Absolute, 25F));
@@ -92,7 +93,7 @@
             tlpTalents.RowStyles.Add(new RowStyle(SizeType.Absolute, 25F));
             tlpTalents.RowStyles.Add(new RowStyle(SizeType.Absolute, 25F));
             tlpTalents.RowStyles.Add(new RowStyle(SizeType.Absolute, 25F));
-            tlpTalents.Size = new Size(319, 228);
+            tlpTalents.Size = new Size(295, 228);
             tlpTalents.TabIndex = 0;
             // 
             // cmbTalent9
@@ -100,9 +101,9 @@
             cmbTalent9.Dock = DockStyle.Fill;
             cmbTalent9.DropDownStyle = ComboBoxStyle.DropDownList;
             cmbTalent9.FormattingEnabled = true;
-            cmbTalent9.Location = new Point(58, 203);
+            cmbTalent9.Location = new Point(60, 203);
             cmbTalent9.Name = "cmbTalent9";
-            cmbTalent9.Size = new Size(258, 23);
+            cmbTalent9.Size = new Size(232, 23);
             cmbTalent9.TabIndex = 25;
             // 
             // cmbTalent8
@@ -110,9 +111,9 @@
             cmbTalent8.Dock = DockStyle.Fill;
             cmbTalent8.DropDownStyle = ComboBoxStyle.DropDownList;
             cmbTalent8.FormattingEnabled = true;
-            cmbTalent8.Location = new Point(58, 178);
+            cmbTalent8.Location = new Point(60, 178);
             cmbTalent8.Name = "cmbTalent8";
-            cmbTalent8.Size = new Size(258, 23);
+            cmbTalent8.Size = new Size(232, 23);
             cmbTalent8.TabIndex = 24;
             // 
             // cmbTalent7
@@ -120,9 +121,9 @@
             cmbTalent7.Dock = DockStyle.Fill;
             cmbTalent7.DropDownStyle = ComboBoxStyle.DropDownList;
             cmbTalent7.FormattingEnabled = true;
-            cmbTalent7.Location = new Point(58, 153);
+            cmbTalent7.Location = new Point(60, 153);
             cmbTalent7.Name = "cmbTalent7";
-            cmbTalent7.Size = new Size(258, 23);
+            cmbTalent7.Size = new Size(232, 23);
             cmbTalent7.TabIndex = 23;
             // 
             // cmbTalent6
@@ -130,9 +131,9 @@
             cmbTalent6.Dock = DockStyle.Fill;
             cmbTalent6.DropDownStyle = ComboBoxStyle.DropDownList;
             cmbTalent6.FormattingEnabled = true;
-            cmbTalent6.Location = new Point(58, 128);
+            cmbTalent6.Location = new Point(60, 128);
             cmbTalent6.Name = "cmbTalent6";
-            cmbTalent6.Size = new Size(258, 23);
+            cmbTalent6.Size = new Size(232, 23);
             cmbTalent6.TabIndex = 22;
             // 
             // cmbTalent5
@@ -140,9 +141,9 @@
             cmbTalent5.Dock = DockStyle.Fill;
             cmbTalent5.DropDownStyle = ComboBoxStyle.DropDownList;
             cmbTalent5.FormattingEnabled = true;
-            cmbTalent5.Location = new Point(58, 103);
+            cmbTalent5.Location = new Point(60, 103);
             cmbTalent5.Name = "cmbTalent5";
-            cmbTalent5.Size = new Size(258, 23);
+            cmbTalent5.Size = new Size(232, 23);
             cmbTalent5.TabIndex = 21;
             // 
             // cmbTalent4
@@ -150,9 +151,9 @@
             cmbTalent4.Dock = DockStyle.Fill;
             cmbTalent4.DropDownStyle = ComboBoxStyle.DropDownList;
             cmbTalent4.FormattingEnabled = true;
-            cmbTalent4.Location = new Point(58, 78);
+            cmbTalent4.Location = new Point(60, 78);
             cmbTalent4.Name = "cmbTalent4";
-            cmbTalent4.Size = new Size(258, 23);
+            cmbTalent4.Size = new Size(232, 23);
             cmbTalent4.TabIndex = 20;
             // 
             // cmbTalent3
@@ -160,9 +161,9 @@
             cmbTalent3.Dock = DockStyle.Fill;
             cmbTalent3.DropDownStyle = ComboBoxStyle.DropDownList;
             cmbTalent3.FormattingEnabled = true;
-            cmbTalent3.Location = new Point(58, 53);
+            cmbTalent3.Location = new Point(60, 53);
             cmbTalent3.Name = "cmbTalent3";
-            cmbTalent3.Size = new Size(258, 23);
+            cmbTalent3.Size = new Size(232, 23);
             cmbTalent3.TabIndex = 19;
             // 
             // cmbTalent2
@@ -170,18 +171,19 @@
             cmbTalent2.Dock = DockStyle.Fill;
             cmbTalent2.DropDownStyle = ComboBoxStyle.DropDownList;
             cmbTalent2.FormattingEnabled = true;
-            cmbTalent2.Location = new Point(58, 28);
+            cmbTalent2.Location = new Point(60, 28);
             cmbTalent2.Name = "cmbTalent2";
-            cmbTalent2.Size = new Size(258, 23);
+            cmbTalent2.Size = new Size(232, 23);
             cmbTalent2.TabIndex = 18;
             // 
             // lblTalent9
             // 
             lblTalent9.AutoSize = true;
             lblTalent9.Dock = DockStyle.Fill;
+            lblTalent9.Font = new Font("Segoe UI", 9F, FontStyle.Bold);
             lblTalent9.Location = new Point(3, 200);
             lblTalent9.Name = "lblTalent9";
-            lblTalent9.Size = new Size(49, 28);
+            lblTalent9.Size = new Size(51, 28);
             lblTalent9.TabIndex = 16;
             lblTalent9.Text = "Talent 9";
             lblTalent9.TextAlign = ContentAlignment.MiddleLeft;
@@ -190,9 +192,10 @@
             // 
             lblTalent8.AutoSize = true;
             lblTalent8.Dock = DockStyle.Fill;
+            lblTalent8.Font = new Font("Segoe UI", 9F, FontStyle.Bold);
             lblTalent8.Location = new Point(3, 175);
             lblTalent8.Name = "lblTalent8";
-            lblTalent8.Size = new Size(49, 25);
+            lblTalent8.Size = new Size(51, 25);
             lblTalent8.TabIndex = 14;
             lblTalent8.Text = "Talent 8";
             lblTalent8.TextAlign = ContentAlignment.MiddleLeft;
@@ -201,9 +204,10 @@
             // 
             lblTalent7.AutoSize = true;
             lblTalent7.Dock = DockStyle.Fill;
+            lblTalent7.Font = new Font("Segoe UI", 9F, FontStyle.Bold);
             lblTalent7.Location = new Point(3, 150);
             lblTalent7.Name = "lblTalent7";
-            lblTalent7.Size = new Size(49, 25);
+            lblTalent7.Size = new Size(51, 25);
             lblTalent7.TabIndex = 12;
             lblTalent7.Text = "Talent 7";
             lblTalent7.TextAlign = ContentAlignment.MiddleLeft;
@@ -212,9 +216,10 @@
             // 
             lblTalent6.AutoSize = true;
             lblTalent6.Dock = DockStyle.Fill;
+            lblTalent6.Font = new Font("Segoe UI", 9F, FontStyle.Bold);
             lblTalent6.Location = new Point(3, 125);
             lblTalent6.Name = "lblTalent6";
-            lblTalent6.Size = new Size(49, 25);
+            lblTalent6.Size = new Size(51, 25);
             lblTalent6.TabIndex = 10;
             lblTalent6.Text = "Talent 6";
             lblTalent6.TextAlign = ContentAlignment.MiddleLeft;
@@ -223,9 +228,10 @@
             // 
             lblTalent5.AutoSize = true;
             lblTalent5.Dock = DockStyle.Fill;
+            lblTalent5.Font = new Font("Segoe UI", 9F, FontStyle.Bold);
             lblTalent5.Location = new Point(3, 100);
             lblTalent5.Name = "lblTalent5";
-            lblTalent5.Size = new Size(49, 25);
+            lblTalent5.Size = new Size(51, 25);
             lblTalent5.TabIndex = 8;
             lblTalent5.Text = "Talent 5";
             lblTalent5.TextAlign = ContentAlignment.MiddleLeft;
@@ -234,9 +240,10 @@
             // 
             lblTalent4.AutoSize = true;
             lblTalent4.Dock = DockStyle.Fill;
+            lblTalent4.Font = new Font("Segoe UI", 9F, FontStyle.Bold);
             lblTalent4.Location = new Point(3, 75);
             lblTalent4.Name = "lblTalent4";
-            lblTalent4.Size = new Size(49, 25);
+            lblTalent4.Size = new Size(51, 25);
             lblTalent4.TabIndex = 6;
             lblTalent4.Text = "Talent 4";
             lblTalent4.TextAlign = ContentAlignment.MiddleLeft;
@@ -245,9 +252,10 @@
             // 
             lblTalent3.AutoSize = true;
             lblTalent3.Dock = DockStyle.Fill;
+            lblTalent3.Font = new Font("Segoe UI", 9F, FontStyle.Bold);
             lblTalent3.Location = new Point(3, 50);
             lblTalent3.Name = "lblTalent3";
-            lblTalent3.Size = new Size(49, 25);
+            lblTalent3.Size = new Size(51, 25);
             lblTalent3.TabIndex = 4;
             lblTalent3.Text = "Talent 3";
             lblTalent3.TextAlign = ContentAlignment.MiddleLeft;
@@ -256,9 +264,10 @@
             // 
             lblTalent2.AutoSize = true;
             lblTalent2.Dock = DockStyle.Fill;
+            lblTalent2.Font = new Font("Segoe UI", 9F, FontStyle.Bold);
             lblTalent2.Location = new Point(3, 25);
             lblTalent2.Name = "lblTalent2";
-            lblTalent2.Size = new Size(49, 25);
+            lblTalent2.Size = new Size(51, 25);
             lblTalent2.TabIndex = 2;
             lblTalent2.Text = "Talent 2";
             lblTalent2.TextAlign = ContentAlignment.MiddleLeft;
@@ -267,9 +276,10 @@
             // 
             lblTalent1.AutoSize = true;
             lblTalent1.Dock = DockStyle.Fill;
+            lblTalent1.Font = new Font("Segoe UI", 9F, FontStyle.Bold);
             lblTalent1.Location = new Point(3, 0);
             lblTalent1.Name = "lblTalent1";
-            lblTalent1.Size = new Size(49, 25);
+            lblTalent1.Size = new Size(51, 25);
             lblTalent1.TabIndex = 0;
             lblTalent1.Text = "Talent 1";
             lblTalent1.TextAlign = ContentAlignment.MiddleLeft;
@@ -279,15 +289,15 @@
             cmbTalent1.Dock = DockStyle.Top;
             cmbTalent1.DropDownStyle = ComboBoxStyle.DropDownList;
             cmbTalent1.FormattingEnabled = true;
-            cmbTalent1.Location = new Point(58, 3);
+            cmbTalent1.Location = new Point(60, 3);
             cmbTalent1.Name = "cmbTalent1";
-            cmbTalent1.Size = new Size(258, 23);
+            cmbTalent1.Size = new Size(232, 23);
             cmbTalent1.TabIndex = 17;
             // 
             // btnLoadSave
             // 
-            btnLoadSave.Font = new Font("Segoe UI", 10F);
-            btnLoadSave.Location = new Point(153, 3);
+            btnLoadSave.Font = new Font("Segoe UI", 10F, FontStyle.Bold);
+            btnLoadSave.Location = new Point(144, 6);
             btnLoadSave.Name = "btnLoadSave";
             btnLoadSave.Size = new Size(163, 28);
             btnLoadSave.TabIndex = 1;
@@ -299,6 +309,7 @@
             // 
             cmbPets.Anchor = AnchorStyles.Top;
             cmbPets.DropDownStyle = ComboBoxStyle.DropDownList;
+            cmbPets.ForeColor = SystemColors.WindowText;
             cmbPets.FormattingEnabled = true;
             cmbPets.Location = new Point(12, 71);
             cmbPets.Name = "cmbPets";
@@ -309,20 +320,19 @@
             // lblLoadedSave
             // 
             lblLoadedSave.AutoSize = true;
-            lblLoadedSave.Font = new Font("Segoe UI", 12F);
-            lblLoadedSave.Location = new Point(3, 5);
+            lblLoadedSave.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
+            lblLoadedSave.Location = new Point(12, 9);
             lblLoadedSave.Name = "lblLoadedSave";
-            lblLoadedSave.Size = new Size(117, 21);
+            lblLoadedSave.Size = new Size(133, 21);
             lblLoadedSave.TabIndex = 3;
-            lblLoadedSave.Text = "No save loaded";
+            lblLoadedSave.Text = "No Save Loaded";
             // 
             // btnSaveToCoreKeeper
             // 
-            btnSaveToCoreKeeper.Dock = DockStyle.Bottom;
-            btnSaveToCoreKeeper.Font = new Font("Segoe UI", 12F);
-            btnSaveToCoreKeeper.Location = new Point(0, 386);
+            btnSaveToCoreKeeper.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
+            btnSaveToCoreKeeper.Location = new Point(12, 383);
             btnSaveToCoreKeeper.Name = "btnSaveToCoreKeeper";
-            btnSaveToCoreKeeper.Size = new Size(319, 46);
+            btnSaveToCoreKeeper.Size = new Size(295, 43);
             btnSaveToCoreKeeper.TabIndex = 4;
             btnSaveToCoreKeeper.Text = "Save";
             btnSaveToCoreKeeper.UseVisualStyleBackColor = true;
@@ -330,7 +340,8 @@
             // 
             // btnCopyFirstTalentToAll
             // 
-            btnCopyFirstTalentToAll.Location = new Point(83, 120);
+            btnCopyFirstTalentToAll.Font = new Font("Segoe UI", 9F, FontStyle.Bold);
+            btnCopyFirstTalentToAll.Location = new Point(148, 123);
             btnCopyFirstTalentToAll.Name = "btnCopyFirstTalentToAll";
             btnCopyFirstTalentToAll.Size = new Size(156, 23);
             btnCopyFirstTalentToAll.TabIndex = 5;
@@ -341,10 +352,10 @@
             // label1
             // 
             label1.AutoSize = true;
-            label1.Font = new Font("Segoe UI", 9F);
+            label1.Font = new Font("Segoe UI", 9F, FontStyle.Bold);
             label1.Location = new Point(12, 53);
             label1.Name = "label1";
-            label1.Size = new Size(58, 15);
+            label1.Size = new Size(64, 15);
             label1.TabIndex = 6;
             label1.Text = "Select Pet";
             // 
@@ -352,6 +363,7 @@
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(128, 128, 255);
             ClientSize = new Size(319, 432);
             Controls.Add(label1);
             Controls.Add(btnCopyFirstTalentToAll);
@@ -360,7 +372,9 @@
             Controls.Add(cmbPets);
             Controls.Add(btnLoadSave);
             Controls.Add(tlpTalents);
+            ForeColor = SystemColors.ActiveCaptionText;
             FormBorderStyle = FormBorderStyle.FixedSingle;
+            Icon = (Icon)resources.GetObject("$this.Icon");
             MaximizeBox = false;
             MinimizeBox = false;
             Name = "MainForm";
